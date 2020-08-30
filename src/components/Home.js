e1f968f21c75dc8e9cec80f9e31265824d62247a
shubhamchauhan4488 <shubhamchauhan4488@gmail.com>
2020-08-30 17:02:45 -0400

    Add other mortgage form feilds and state

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,10 +4,15 @@ import { MortgageForm } from "./MortgageForm";
 export const Home = () => {
   const [mortgageAmt, setMortgageAmt] = useState(50000);
   const [rate, setRate] = useState(5);
-
+  const [amortizationPeriod, setAmortizationPeriod] = useState(25);
+  const [paymentFrequency, setPaymentFrequency] = useState(12);
+  const [term, setTerm] = useState(5);
   const [errors, setErrors] = useState({
     mortgageAmt: "",
     rate: "",
+    amortizationPeriod: "",
+    paymentFrequency: "",
+    term: "",
   });
 
   const handleInputChange = (e) => {
@@ -24,6 +29,18 @@ export const Home = () => {
         setRate(e.target.value);
         break;
       }
+      case "amortizationPeriod": {
+        setAmortizationPeriod(e.target.value);
+        break;
+      }
+      case "paymentFrequency": {
+        setPaymentFrequency(e.target.value);
+        break;
+      }
+      case "term": {
+        setTerm(e.target.value);
+        break;
+      }
       default:
         return;
     }
@@ -34,6 +51,9 @@ export const Home = () => {
       <MortgageForm
         mortgageAmt={mortgageAmt}
         rate={rate}
+        amortizationPeriod={amortizationPeriod}
+        paymentFrequency={paymentFrequency}
+        term={term}
         handleInputChange={handleInputChange}
         errors={errors}
       />
--- src/components/MortgageForm.js
+++ src/components/MortgageForm.js
@@ -1,6 +1,7 @@
 import React from "react";
+import { paymentFrequencyOptions, getTermOptions } from "../constants/PaymentPlanOptions";
 
-export const MortgageForm = ({ mortgageAmt, rate, handleInputChange, errors }) => {
+export const MortgageForm = ({ mortgageAmt, rate, amortizationPeriod, paymentFrequency, term, handleInputChange, errors }) => {
   return (
     <div id="mortgage-form-container" data-testid="mortgage-form-container" className="jumbotron no-gutters col-12 col-sm-6 mt-3 py-5">
       <div className="row align-items-center mb-3">
@@ -49,6 +50,70 @@ export const MortgageForm = ({ mortgageAmt, rate, handleInputChange, errors }) =
               onChange={handleInputChange}
             />
           </div>
+        </div>
+      </div>
+
+      <div className="row align-items-center mb-3">
+        <div className="col-12 col-sm-6">
+          <label htmlFor="amortizationPeriod">Amortization Period</label>
+        </div>
+        <div className="col-12 col-sm-6">
+          <input
+            id="amortizationPeriod"
+            data-testid="amortizationPeriod"
+            type="number"
+            className="form-control"
+            name="amortizationPeriod"
+            aria-label="Amortization Period"
+            placeholder="Years"
+            value={amortizationPeriod}
+            onChange={handleInputChange}
+            max={30}
+            min={1}
+          />
+        </div>
+      </div>
+
+      <div className="row align-items-center mb-3">
+        <div className="col-12 col-sm-6">
+          <label htmlFor="paymentFrequency">Payment Frequency</label>
+        </div>
+        <div className="col-12 col-sm-6">
+          <select
+            name="paymentFrequency"
+            id="paymentFrequency"
+            data-testid="paymentFrequency"
+            value={paymentFrequency}
+            onChange={handleInputChange}
+          >
+            {paymentFrequencyOptions.map((option, index) => (
+              <option id={index} value={option.value} key={index}>
+                {option.label}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+
+      {/** Amortisation Input */}
+      <div className="row align-items-center mb-3">
+        <div className="col-12 col-sm-6">
+          <label htmlFor="term">Term</label>
+        </div>
+        <div className="col-12 col-sm-6">
+          <select
+            id="term"
+            data-testid="term"
+            name="term"
+            value={term}
+            onChange={handleInputChange}
+          >
+            {getTermOptions().map((option, index) => (
+              <option id={index} value={option.value} key={index}>
+                {option.label}
+              </option>
+            ))}
+          </select>
         </div>
       </div>
     </div>
