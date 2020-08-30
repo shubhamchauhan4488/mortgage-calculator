6505fdf099ac9966385db841be5e36ef116084c9
shubhamchauhan4488 <shubhamchauhan4488@gmail.com>
2020-08-30 19:14:20 -0400

    Make charts responsive and add styles

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -14,49 +14,49 @@ import { colors } from "../globals/theme";
 
 export const Charts = (props) => {
   return (
-    <>
-      <div className="container-fluid">
-        <h2>Payment Diagrams</h2>
-        <div className="row justify-content-center">
-          <AreaChart
-            width={500}
-            height={250}
-            data={props.graphData}
-          >
-            <defs>
-              <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="5%" stopColor={colors.PURPLE} stopOpacity={0.8} />
-                <stop offset="95%" stopColor={colors.PURPLE} stopOpacity={0.1} />
-              </linearGradient>
-            </defs>
-            <XAxis dataKey="name" tick={{ stroke: colors.PRIMARY }} />
-            <YAxis tick={{ stroke: colors.PRIMARY }} />
-            <CartesianGrid strokeDasharray="3 3" />
-            <Tooltip />
-            <Area
-              type="monotone"
-              dataKey="principal"
-              stroke={colors.PURPLE}
-              fillOpacity={1}
-              fill="url(#colorUv)"
-            />
-          </AreaChart>
+    <div className="container-fluid">
+      <h2 className="diagram-heading">Payment Diagrams</h2>
+      <div className="row justify-content-center justify-content-md-around mb-3 mt-3">
+        <AreaChart
+          className='chart-container'
+          width={500}
+          height={250}
+          data={props.graphData}
+        >
+          <defs>
+            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor={colors.PURPLE} stopOpacity={0.8} />
+              <stop offset="95%" stopColor={colors.PURPLE} stopOpacity={0.1} />
+            </linearGradient>
+          </defs>
+          <XAxis dataKey="name" tick={{ stroke: colors.PRIMARY }} />
+          <YAxis tick={{ stroke: colors.PRIMARY }} />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <Area
+            type="monotone"
+            dataKey="principal"
+            stroke={colors.PURPLE}
+            fillOpacity={1}
+            fill="url(#colorUv)"
+          />
+        </AreaChart>
 
-          <BarChart
-            width={300}
-            height={250}
-            data={props.barData}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="totalCost" tick={{ stroke: colors.PRIMARY }} />
-            <YAxis tick={{ stroke: colors.PRIMARY }} />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="principal" fill={colors.PURPLE} />
-            <Bar dataKey="interest" fill={colors.LIGHT_GREEN} />
-          </BarChart>
-        </div>
+        <BarChart
+          className='chart-container'
+          width={300}
+          height={250}
+          data={props.barData}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="totalCost" tick={{ stroke: colors.PRIMARY }} />
+          <YAxis tick={{ stroke: colors.PRIMARY }} />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="principal" fill={colors.PURPLE} />
+          <Bar dataKey="interest" fill={colors.LIGHT_GREEN} />
+        </BarChart>
       </div>
-    </>
+    </div>
   );
 };
